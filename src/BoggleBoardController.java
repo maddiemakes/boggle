215ce042fc96315811ad33361e4c2c926704c3d9
Jordan <jwells.me@gmail.com>
2016-04-07 13:44:01 -0400

    Letter selection updated, colors updated, buttons added (check TODO for more)

--- src/BoggleBoardController.java
+++ src/BoggleBoardController.java
@@ -3,14 +3,27 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
+import javafx.scene.control.Label;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class BoggleBoardController {
 
     Integer colIndex;
     Integer rowIndex;
+    Integer[][] availableLetters = new Integer[4][4];
+    List<Node> clickedLetters = new ArrayList<>();
+    Pair<Integer, Integer> lastClicked = new Pair<>(null, null);
+    List<String> dice = new ArrayList<String>(16) {{
+        add("AAEEGN"); add("ABBJOO"); add("ACHOPS");
+        add("AFFKPS"); add("AOOTTW"); add("CIMOTU");
+        add("DEILRX"); add("DELRVY"); add("DISTTY");
+        add("EEGHNW"); add("EEINSU"); add("EHRTVW");
+        add("EIOSST"); add("ELRTTY"); add("HIMNUQ");
+        add("HLNNRZ");
+    }};
 
     @FXML
     private ResourceBundle resources;
@@ -18,34 +31,154 @@ public class BoggleBoardController {
     @FXML
     private URL location;
 
-    private void mouseEvent(ActionEvent e) {
-        Node source = (Node)e.getSource();
+    @FXML
+    private GridPane gridPane;
+
+
+    private void mouseEvent(MouseEvent e, Node source) {
         colIndex = GridPane.getColumnIndex(source);
         rowIndex = GridPane.getRowIndex(source);
-//        if(colIndex == null)
-//            colIndex = 0;
-//        if(rowIndex == null)
-//            rowIndex = 0;
+        if(colIndex == null) {
+            colIndex = 0;
+        }
+        if(rowIndex == null) {
+            rowIndex = 0;
+        }
+    }
+
+    private boolean isValidLetter(MouseEvent e, Node source) {
+        if (availableLetters[rowIndex][colIndex] == 1) {
+            if (lastClicked.getKey() != null && lastClicked.getValue() != null) {
+                if (!(rowIndex == lastClicked.getKey()
+                        && colIndex == lastClicked.getValue())
+                        && (((rowIndex == (lastClicked.getKey() - 1))
+                            || (rowIndex == lastClicked.getKey())
+                            || (rowIndex == (lastClicked.getKey() + 1)))
+                          && ((colIndex == (lastClicked.getValue() - 1))
+                              || (colIndex == lastClicked.getValue())
+                              || (colIndex == (lastClicked.getValue() + 1))))) {
+                    return true;
+                }
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
     }
 
     @FXML
     void handleOnMouseClicked(MouseEvent event) {
-//        mouseEvent(event);
         Node source = (Node)event.getSource();
-        source.setStyle("-fx-background-color:rgba(255,0,0,1)");
+        mouseEvent(event, source);
+        if (isValidLetter(event, source)) {
+            clickedLetters.add(source);
+            availableLetters[rowIndex][colIndex] = 0;
+            lastClicked = new Pair<>(rowIndex, colIndex);
+            source.setStyle("-fx-background-color:rgba(40,80,255,1); -fx-opacity:0.9;");
+        }
+    }
+
+    @FXML
+    void handleOnMouseEnter(MouseEvent event) {
+        Node source = (Node) event.getSource();
+        mouseEvent(event, source);
+        if (isValidLetter(event, source)) {
+            source.setStyle("-fx-background-color:rgba(90,255,110,1); -fx-opacity:0.8;");
+        }
+        else {
+            for (Node node: clickedLetters) {
+                if (source == node) {
+                    return;
+                }
+            }
+            source.setStyle("-fx-background-color:rgba(255,90,90,1); -fx-opacity:0.8;");
+        }
+    }
+
+    @FXML
+    void handleOnMouseExit(MouseEvent event) {
+        Node source = (Node) event.getSource();
+        mouseEvent(event, source);
+        if (availableLetters[rowIndex][colIndex] == 1) {
+            source.setStyle("-fx-background-color: rgba(245,245,245,1); -fx-opacity: 0.8;");
+        }
 
     }
 
-    //TODO
-    //On mouse hover, check to see if this is a valid letter that you can click.
-    //   If not, turn it red and don't allow clicking. Else turn green.
-    //On mouse click, add the letter to the word
-    //
+    @FXML
+    void handleClearWord(ActionEvent event) {
+        for(int k = 0; k < 4; k++) {
+            for(int j = 0; j < 4; j++) {
+                availableLetters[k][j] = 1;
+            }
+        }
+        clickedLetters.clear();
+        lastClicked = new Pair<>(null, null);
+        for (Node node: gridPane.getChildren()) {
+            node.setStyle("-fx-background-color: rgba(245,245,245,1); -fx-opacity: 0.8;");
+        }
+    }
 
+    //save word button
+    //TODO
+    //points
+    @FXML
+    void handleSaveWord(ActionEvent event) {
+        for (Node node: clickedLetters) {
+            Label label = (Label)node;
+            System.out.print(label.getText());
+        }
+        System.out.println("");
+        handleClearWord(event);
+    }
 
     @FXML
-    void initialize() {
+    void handleNewGame(ActionEvent event) {
+        initialize();
+        handleClearWord(event);
+    }
 
+
+    //the "jumble" simulates actual boggle, using real die letters
+    //  and not repeating dice. additionally, the letter from
+    //  each die is random
+    @FXML
+    void initialize() {
+        Random rand = new Random();
+        for(int k = 0; k < 4; k++) {
+            for(int j = 0; j < 4; j++) {
+                availableLetters[k][j] = 1;
+            }
+        }
+
+        //this prevents duplicate die numbers
+        //list1 is a list of die numbers
+        //list2 is the list of which die each grid cell will be
+        List<Integer> list1 = new ArrayList<Integer>(){{
+            add(0); add(1); add(2); add(3);
+            add(4); add(5); add(6); add(7);
+            add(8); add(9); add(10); add(11);
+            add(12); add(13); add(14); add(15);
+        }};
+        List<Integer> list2 = new ArrayList<>();
+        for (int k = 16; k > 0; k--) {
+            int die = rand.nextInt(k);
+            list2.add(list1.get(die));
+            list1.remove(die);
+        }
+
+        //Jumble
+        //this shakes the box to jumble the letters
+        int k = 0;
+        for (Node node: gridPane.getChildren()) {
+            Label label = (Label)node;
+            label.setText("" + (dice.get(list2.get(k))).charAt(rand.nextInt(6)));
+            if (label.getText().equals("Q")) {
+                label.setText("Qu");
+            }
+            k++;
+        }
     }
 
     @FXML
