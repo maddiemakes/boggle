dcfc3120c8e8ebce9e122b306829b45cdcf18ddd
Jordan <jwells.me@gmail.com>
2016-04-13 10:14:08 -0400

    Started trying AI (not working)

--- src/BoggleBoardController.java
+++ src/BoggleBoardController.java
@@ -13,12 +13,14 @@ import javafx.stage.Stage;
 import javafx.util.Pair;
 import javafx.scene.control.Label;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
+import java.util.List;
 
 public class BoggleBoardController {
 
@@ -42,6 +44,15 @@ public class BoggleBoardController {
     File dictFile = new File(url.getPath());
     boolean newDict = true;
 
+    //AI state variables (testing)
+    Integer AIcolIndex;
+    Integer AIrowIndex;
+    Integer[][] AIavailableLetters = new Integer[4][4];
+    List<Node> AIclickedLetters = new ArrayList<>();
+    Pair<Integer, Integer> AIlastClicked = new Pair<>(null, null);
+    ArrayList<String> AIusedWords = new ArrayList<>();
+    Integer AIletterCount = 0;
+
     @FXML
     private ResourceBundle resources;
 
@@ -61,18 +72,130 @@ public class BoggleBoardController {
     private VBox wordHistoryVBox;
 
 
-    private void mouseEvent(MouseEvent e, Node source) {
-        colIndex = GridPane.getColumnIndex(source);
-        rowIndex = GridPane.getRowIndex(source);
-        if(colIndex == null) {
-            colIndex = 0;
+    //TODO
+    //AI
+    //  - Makes all possible guesses? (most likely will do this)
+    //  - Tries all words in dictionary?
+    //
+    //First get it to print all possible combinations, then check them for valid words
+
+    @FXML
+    void handleAI(ActionEvent event) {
+        //This should prevent it from trying to make words larger than 16 letters
+        if (AIletterCount < 16) {
+            //for each letter (gridpane)
+            for (Node node : gridPane.getChildren()) {
+
+                //set location of letter
+                AIcolIndex = GridPane.getColumnIndex(node);
+                AIrowIndex = GridPane.getRowIndex(node);
+                if (AIcolIndex == null) {
+                    AIcolIndex = 0;
+                }
+                if (AIrowIndex == null) {
+                    AIrowIndex = 0;
+                }
+
+                //if this is a valid letter {
+                if (AIavailableLetters[AIrowIndex][AIcolIndex] == 1) {
+                    if (AIlastClicked.getKey() != null && AIlastClicked.getValue() != null) {
+                        if (!(AIrowIndex == AIlastClicked.getKey()
+                                && AIcolIndex == AIlastClicked.getValue())
+                                && (((AIrowIndex == (AIlastClicked.getKey() - 1))
+                                || (AIrowIndex == AIlastClicked.getKey())
+                                || (AIrowIndex == (AIlastClicked.getKey() + 1)))
+                                && ((AIcolIndex == (AIlastClicked.getValue() - 1))
+                                || (AIcolIndex == AIlastClicked.getValue())
+                                || (AIcolIndex == (AIlastClicked.getValue() + 1))))) {
+
+                            //do this stuff if valid letter
+                            AIavailableLetters[AIcolIndex][AIrowIndex] = 0;
+                            AIclickedLetters.add(node);
+                            AIlastClicked = new Pair<>(AIcolIndex, AIrowIndex);
+
+                            //sets the word
+                            String word = "";
+                            for (Node letter : AIclickedLetters) {
+                                Label label = (Label) letter;
+                                word += label.getText();
+                            }
+
+                            //checks to see if word is in dictionary or has been used before
+                            boolean wordFound = false;
+                            for (String line : dict) {
+                                if (word.equalsIgnoreCase(line)) {
+                                    for (String newWord : AIusedWords) {
+                                        if (word.equalsIgnoreCase(newWord)) {
+                                            wordFound = true;
+                                        }
+                                    }
+                                    //if we haven't used the word before, add it and print it
+                                    if (!wordFound) {
+                                        AIusedWords.add(word);
+                                        System.out.println(word);
+                                    }
+                                }
+                            }
+                            //increment the letter count and recurse
+                            AIletterCount++;
+                            handleAI(event);
+                        }
+                    }
+                    //this is here for restarting the word check, in which "last clicked" is null
+                    else {
+                        AIavailableLetters[AIcolIndex][AIrowIndex] = 0;
+                        AIclickedLetters.add(node);
+                        AIlastClicked = new Pair<>(AIcolIndex, AIrowIndex);
+                        String word = "";
+                        for (Node letter : AIclickedLetters) {
+                            Label label = (Label) letter;
+                            word += label.getText();
+                        }
+                        boolean wordFound = false;
+                        for (String line : dict) {
+                            if (word.equalsIgnoreCase(line)) {
+                                for (String newWord : AIusedWords) {
+                                    if (word.equalsIgnoreCase(newWord)) {
+                                        wordFound = true;
+                                    }
+                                }
+                                if (!wordFound) {
+                                    AIusedWords.add(word);
+                                    System.out.println(word);
+                                }
+                            }
+                        }
+                        AIletterCount++;
+                        handleAI(event);
+                    }
+                }
+            }
         }
-        if(rowIndex == null) {
-            rowIndex = 0;
+        //if we've used 16 letters, reset the variables and try again
+        else {
+            AIletterCount = 0;
+            for(int k = 0; k < 4; k++) {
+                for(int j = 0; j < 4; j++) {
+                    AIavailableLetters[k][j] = 1;
+                }
+            }
+            AIclickedLetters.clear();
+            AIlastClicked = new Pair<>(null, null);
+            handleAI(event);
         }
     }
 
-    private boolean isValidLetter(MouseEvent e, Node source) {
+    void addToDict() {
+        try(BufferedReader br = new BufferedReader(new FileReader(dictFile))) {
+            for(String line; (line = br.readLine()) != null; ) {
+                dict.add(line);
+            }
+        }
+        catch (IOException ex){}
+        newDict = false;
+    }
+
+    private boolean isValidLetter() {
         if (availableLetters[rowIndex][colIndex] == 1) {
             if (lastClicked.getKey() != null && lastClicked.getValue() != null) {
                 if (!(rowIndex == lastClicked.getKey()
@@ -93,18 +216,22 @@ public class BoggleBoardController {
         return false;
     }
 
-    //TODO
-    //AI
-    //  - Makes all possible guesses? (most likely will do this)
-    //  - Tries all words in dictionary?
-    //
-    //First get it to print all possible combinations, then check them for valid words
+    private void mouseEvent(MouseEvent e, Node source) {
+        colIndex = GridPane.getColumnIndex(source);
+        rowIndex = GridPane.getRowIndex(source);
+        if(colIndex == null) {
+            colIndex = 0;
+        }
+        if(rowIndex == null) {
+            rowIndex = 0;
+        }
+    }
 
     @FXML
     void handleOnMouseClicked(MouseEvent event) {
         Node source = (Node)event.getSource();
         mouseEvent(event, source);
-        if (isValidLetter(event, source)) {
+        if (isValidLetter()) {
             clickedLetters.add(source);
             availableLetters[rowIndex][colIndex] = 0;
             lastClicked = new Pair<>(rowIndex, colIndex);
@@ -116,7 +243,7 @@ public class BoggleBoardController {
     void handleOnMouseEnter(MouseEvent event) {
         Node source = (Node) event.getSource();
         mouseEvent(event, source);
-        if (isValidLetter(event, source)) {
+        if (isValidLetter()) {
             source.setStyle("-fx-background-color:rgba(90,255,110,1); -fx-opacity:0.8;");
         }
         else {
@@ -237,16 +364,20 @@ public class BoggleBoardController {
         wordHistoryVBox.getChildren().clear();
     }
 
-    void addToDict() {
-        try(BufferedReader br = new BufferedReader(new FileReader(dictFile))) {
-            for(String line; (line = br.readLine()) != null; ) {
-                dict.add(line);
-            }
-        }
-        catch (IOException ex){}
-        newDict = false;
+    @FXML
+    public void handleChangeDictionary(ActionEvent event) {
+        FileChooser fc = new FileChooser();
+        dictFile = fc.showOpenDialog(null);
+        newDict = true;
+        //TODO
+        //alert user that this will start a new game.
+        handleNewGame(event);
     }
 
+    @FXML
+    private void handleClose(ActionEvent event) {
+        System.exit(0);
+    }
 
     //the "jumble" simulates actual boggle, using real die letters
     //  and not repeating dice. additionally, the letter from
@@ -257,6 +388,11 @@ public class BoggleBoardController {
         for(int k = 0; k < 4; k++) {
             for(int j = 0; j < 4; j++) {
                 availableLetters[k][j] = 1;
+
+                //TODO
+                //remove this
+                AIavailableLetters[k][j] = 1;
+
             }
         }
         points = 0;
@@ -297,20 +433,5 @@ public class BoggleBoardController {
             dict.clear();
             addToDict();
         }
-    }
-
-    @FXML
-    private void handleClose(ActionEvent event) {
-        System.exit(0);
-    }
-
-    @FXML
-    public void handleChangeDictionary(ActionEvent event) {
-        FileChooser fc = new FileChooser();
-        dictFile = fc.showOpenDialog(null);
-        newDict = true;
-        //TODO
-        //alert user that this will start a new game.
-        handleNewGame(event);
     }
 }
